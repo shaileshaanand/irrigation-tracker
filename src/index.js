c056146689f8ad3c03c9db5e8d5f9d7591e54037
Shailesh Aanand <anaandshailu@gmail.com>
2022-05-23 14:51:01 +0530

    add Procfile and message

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const dbConnect = require("./helpers/dbConnect");
 const start = async () => {
   const mongo_uri = process.env.MONGO_URI;
   const port = process.env.PORT || 3000;
+  console.log(`Connecting to MongoDB at ${mongo_uri}`);
   await dbConnect(mongo_uri);
   console.log(`Connected to MongoDB at ${mongo_uri}`);
   await app.listen(port);
