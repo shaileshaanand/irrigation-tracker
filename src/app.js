aff33e1e0378ceb4713187e612d526badbe6b3eb
Shailesh Aanand <anaandshailu@gmail.com>
2022-05-25 15:34:51 +0530

    add cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
+var cors = require("cors");
 const express = require("express");
+
 require("express-async-errors");
 
 const errorHandlerMiddleware = require("./middleware/errorHandler");
@@ -8,6 +10,7 @@ const powerEventRouter = require("./routes/PowerEvent");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", async (req, res) => {
   res.send("Hello World!");
